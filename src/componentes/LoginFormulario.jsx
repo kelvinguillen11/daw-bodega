bab5c09deef63d0e1a516c5dfaf7cf0aa89b8abf
Katherine109 <avila13yamileth@gmail.com>
2023-10-18 17:43:32 -0600

    subiendo login

--- src/componentes/LoginFormulario.jsx
+++ src/componentes/LoginFormulario.jsx
@@ -1,12 +1,26 @@
 import '../hojas-de-estilo/loginFormulario.css';
-function LoginFormulario(){
-return(
-<>
-<div className="contenedor-Formulario">
-<div className='text-red'>Hola aqui ira el formulario.</div>
-</div>
-</>
-);
+import Registro from './Registro';
+
+function LoginFormulario() {
+  return (
+    <>
+      <div className="contenedor-Formulario">
+        <Registro />
+        <label>Inicio de Sesión</label>
+        <form>
+          <div>
+            <label htmlFor="cuenta">Ingrese su cuenta:</label>
+            <input type="text" id="cuenta" name="cuenta" />
+          </div>
+          <div>
+            <label htmlFor="contrasenia">Ingrese su contraseña:</label>
+            <input type="password" id="contrasenia" name="contrasenia" />
+          </div>
+          <button type="button">Aceptar</button>
+        </form>
+      </div>
+    </>
+  );
 }
 
 export default LoginFormulario;
