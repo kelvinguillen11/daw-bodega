13bee146d5e887b9a871389155b559b0097806ba
kelvigg11 <kelvinalfaro@outlook.es>
2023-10-22 01:49:30 -0600

    Agregando Area de trabajo principal

--- src/componentes/AreaTrabajoPrincipal.jsx
+++ src/componentes/AreaTrabajoPrincipal.jsx
@@ -1,6 +1,19 @@
+import "../hojas-de-estilo/areaTrabajoPrincipal.css";
 function AreaTrabajoPrincipal(){
     return(
         <>
+        <header className="container-xxl bg dark header-Principal bg-dark text-white">
+        <div className="">
+        <h4>Bienvenido, usuario 32</h4>
+        </div>
+        <div>
+            <button type="button" className="btn btn-primary">Salir</button>
+        </div>
+        </header>
+        <main className="Contenedor-trabajo-Principal">
+    
+
+        </main>
         </>
     );
 }
