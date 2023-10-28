5b5f9a0dc74fd37d686cde5703f8a6e43afb058f
kelvigg11 <kelvinalfaro@outlook.es>
2023-10-27 20:04:30 -0600

    Agregando react-hook-form

--- src/componentes/LoginFormulario.jsx
+++ src/componentes/LoginFormulario.jsx
@@ -4,29 +4,39 @@ import VideoPrincipal from './VideoPrincipal';
 import { Link} from 'react-router-dom';
 import {BiUser,BiLockAlt} from "react-icons/bi";
 import FooterPrincipal from './FooterPrincipal';
+import { useForm } from 'react-hook-form';
+
+
 function LoginFormulario() {
+  const {register,handleSubmit} = useForm();
+  const Submit = ()=>{
+   
+  }
   return (
     <>
     <HeaderPrincipal/>
     <VideoPrincipal/>
     <div className='Contenedor-Login-Principal'>
     <div className='Login-wrapper'>
-    <form action=''> 
+    <form onSubmit={handleSubmit(Submit)}> 
     <h1>Login</h1>
     <div className='input-box'>
-    <input type='text' placeholder='Usuario' required /> 
+    <input type='text' placeholder='Usuario' {...register('userLogin',{
+      required: true
+    })} required /> 
     <i><BiUser/></i>
     </div>
     <div className='input-box'>
-    <input type='password' placeholder='password' required />
+    <input type='password' placeholder='password' {...register('userPassword',{
+      required: true
+    })} />
     <i><BiLockAlt/></i> 
     </div>
     <div className='recuerda-olvidado'>
     <label><input type='checkbox'/> Recuerdame</label>
     <a href='#'>olvidaste tu password?</a>
     </div>
-    <Link to="/Area-Trabajo"><button type='submit' className='btn'>Login</button></Link>
-
+    <button type='submit' className='btn'>Login</button>
     <div className='link-registro'>
     <p>No tienes una cuenta? <Link to="/registro">Registrate</Link></p>
     </div>
